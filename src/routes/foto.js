8a4c1904dfea26fa6571d2f08b3e1ff145308c69
joao1-barbosa <joao.rbarbosa3@gmail.com>
2023-03-16 20:32:20 -0300

    Upload handler configDD

--- src/routes/foto.js
+++ src/routes/foto.js
@@ -1,8 +1,9 @@
 import { Router } from 'express';
 import fotoController from '../controllers/Foto';
+import loginRequired from '../middlewares/loginRequired';
 
 const router = new Router();
 
-router.post('/', fotoController.create);
+router.post('/', loginRequired, fotoController.create);
 
 export default router;
